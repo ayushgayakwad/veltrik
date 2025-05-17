4880b985a0e21cf6302c61dc1b7d2c37b8a6377d
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2025-05-17 17:08:27 +0530

    Updates

--- quiz-objective.php
+++ quiz-objective.php
@@ -198,6 +198,14 @@ if ($time_left <= 0) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TT16L01ZJB"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TT16L01ZJB');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Electric Vehicle Quiz</title>
